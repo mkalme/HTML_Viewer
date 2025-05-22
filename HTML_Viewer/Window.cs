76d831d71ff8fb5afec80e3f6c11200e310eb598
mkalme <mikelis.kalme@gmail.com>
2025-05-22 18:03:24 +0300

    Add project files.

---
+++ HTML_Viewer/Window.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+
+namespace HTML_Viewer {
+    public partial class Window : Form {
+        private readonly string FilePath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\htmlTest.html";
+        
+        public Window()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            WebBrowser.DocumentText = File.ReadAllText(FilePath);
+        }
+    }
+}
